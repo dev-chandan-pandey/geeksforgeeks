b77cdf8a67f73d9020190aa4c9421ae393f3ad5f
dev-chandan-pandey <147697821+dev-chandan-pandey@users.noreply.github.com>
2025-05-30 22:21:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Closest Neighbour in BST/closest-neighbour-in-bst.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMaxFork(Node root, int k) {
+        int res = -1;
+        while (root != null) {
+            if (root.data == k) return k;
+            if (root.data < k) {
+                res = root.data;
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+        return res;
+    }
+}
