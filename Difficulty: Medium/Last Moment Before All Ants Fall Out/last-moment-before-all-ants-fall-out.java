d2ca042af905aa89beab16b95141e84e0db8991a
dev-chandan-pandey <147697821+dev-chandan-pandey@users.noreply.github.com>
2025-07-24 22:39:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Last Moment Before All Ants Fall Out/last-moment-before-all-ants-fall-out.java
+++ Difficulty: Medium/Last Moment Before All Ants Fall Out/last-moment-before-all-ants-fall-out.java
@@ -1,8 +1,7 @@
 class Solution {
-    public int getLastMoment(int n, int left[], int right[]) {
-        int maxTime = 0;
-        for (int p : left) maxTime = Math.max(maxTime, p);
-        for (int p : right) maxTime = Math.max(maxTime, n - p);
-        return maxTime;
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int maxLeft = Arrays.stream(left).max().orElse(0);
+        int maxRight = n - Arrays.stream(right).min().orElse(n);
+        return Math.max(maxLeft, maxRight);
     }
 }
