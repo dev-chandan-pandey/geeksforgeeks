9728e247052dc0735f4880f2712464b79c8cba00
dev-chandan-pandey <147697821+dev-chandan-pandey@users.noreply.github.com>
2025-08-18 22:08:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.java
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -1,54 +1,14 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int test_cases = Integer.parseInt(br.readLine().trim());
-
-        while (test_cases-- > 0) {
-            // Read the array from input line
-            String[] input = br.readLine().trim().split(" ");
-            int[] arr = new int[input.length];
-            for (int i = 0; i < input.length; i++) {
-                arr[i] = Integer.parseInt(input[i]);
-            }
-
-            // Solution instance to invoke the function
-            Solution ob = new Solution();
-            int result = ob.hIndex(arr);
-
-            System.out.println(result);
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
-    // Function to find hIndex
     public int hIndex(int[] citations) {
-        // code here
-        int len=citations.length;
+        int n = citations.length;
+        int[] bucket = new int[n + 1];
         
-        int[] bucket = new int[len+1];
+        for (int c : citations) bucket[Math.min(c, n)]++;
         
-        for(int c: citations){
-            bucket[Math.min(c, len)]++;
-        }
-        
-        int count=0;
-        for(int i=len;i>=0;i--){
-            count+=bucket[i];
-            if(count >= i){
-                return i;
-            }
+        int count = 0;
+        for (int i = n; i >= 0; i--) {
+            count += bucket[i];
+            if (count >= i) return i;
         }
         return 0;
     }
