65b9154dd44979f15195f1eb4a495d4cf33c4994
dev-chandan-pandey <147697821+dev-chandan-pandey@users.noreply.github.com>
2025-08-27 22:46:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
@@ -1,47 +1,16 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-            System.out.println(new Solution().countTriangles(arr));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    // Function to count the number of possible triangles.
-    // code for c++ and java is same so I am providing only java code.
-    static int countTriangles(int arr[]) {
-        // code here
-        Arrays.sort(arr);
-        int ans=0;
-        for(int i=arr.length-1;i>=0;i--){
-            int st=0, end=i-1;
-            while(st<end){
-                if(arr[st]+arr[end]>arr[i]){
-                    ans+=end-st;
-                    end--;
-                }
-                else {
-                    st++;
-                }
+    public int countTriangles(int[] a) {
+        Arrays.sort(a);
+        int c = 0, n = a.length;
+        for (int k = n - 1; k >= 2; --k) {
+            int i = 0, j = k - 1;
+            while (i < j) {
+                if (a[i] + a[j] > a[k]) {
+                    c += j - i;
+                    --j;
+                } else ++i;
             }
         }
-        return ans;
+        return c;
     }
 }
