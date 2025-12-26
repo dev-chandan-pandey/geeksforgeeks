e6239857a196315c9ffc982bd6141b6eb3cc9f63
dev-chandan-pandey <147697821+dev-chandan-pandey@users.noreply.github.com>
2025-12-26 22:55:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
@@ -1,48 +1,24 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-            String[] nk = br.readLine().trim().split(" ");
-            int k = Integer.parseInt(nk[0]);
-            Solution sln = new Solution();
-            int ans = sln.kthMissing(a, k);
-
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
 class Solution {
     public int kthMissing(int[] arr, int k) {
         // code here
-        int lo=0, hi = arr.length-1;
-        int ans = arr.length + k;
-        while(lo <= hi){
-            int mid = (lo+hi)/2;
-            if(arr[mid] > mid + k){
-                hi = mid - 1;
-                ans = mid + k;
-            }
-            else {
-                lo = mid + 1;
+         // insert all array elements into a set
+        Set<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        int count = 0, curr = 0;
+
+        // loop until we find the k-th missing number
+        while (count < k) {
+            curr++;
+            
+            // increment missing count if current 
+            // element is missing
+            if (!set.contains(curr)) {
+                count++;
             }
         }
-        return ans;
+        return curr;
     }
 }
