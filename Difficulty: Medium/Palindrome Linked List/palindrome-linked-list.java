24a607b421bce1dbc6296092febca2f27ed091f8
dev-chandan-pandey <147697821+dev-chandan-pandey@users.noreply.github.com>
2025-12-31 20:06:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
@@ -0,0 +1,63 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    
+    
+      // Function to reverse a linked list
+    static Node reverseList(Node head) {
+        Node prev = null;
+        Node curr = head;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    // Function to check if two lists are identical
+    static boolean isIdentical(Node n1, Node n2) {
+        while (n1 != null && n2 != null) {
+            if (n1.data != n2.data)
+                return false;
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        return true;
+    }
+    public boolean isPalindrome(Node head) {
+        // code here
+             if (head == null || head.next == null)
+            return true;
+
+        Node slow = head, fast = head;
+
+        while (fast.next != null 
+               && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node head2 = reverseList(slow.next);
+        slow.next = null;
+
+        boolean ret = isIdentical(head, head2);
+
+        head2 = reverseList(head2);
+        slow.next = head2;
+
+        return ret;
+    }
+}
