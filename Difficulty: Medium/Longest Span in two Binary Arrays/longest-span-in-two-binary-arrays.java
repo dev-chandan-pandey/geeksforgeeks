918b0f5f4b1701d5e951adc14657e38d50ee3da2
dev-chandan-pandey <147697821+dev-chandan-pandey@users.noreply.github.com>
2026-02-24 22:48:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Span in two Binary Arrays/longest-span-in-two-binary-arrays.java
+++ Difficulty: Medium/Longest Span in two Binary Arrays/longest-span-in-two-binary-arrays.java
@@ -1,18 +1,13 @@
 class Solution {
-    public int longestCommonSum(int[] arr1, int[] arr2) {
-        int n = arr1.length, sum1 = 0, sum2 = 0, res = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            sum1 += arr1[i];
-            sum2 += arr2[i];
-            int diff = sum1 - sum2;
-            if (diff == 0)
-                res = i + 1;
-            else if (map.containsKey(diff))
-                res = Math.max(res, i - map.get(diff));
-            else
-                map.put(diff, i);
+    public int equalSumSpan(int[] a1, int[] a2) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        mp.put(0, -1);
+        int diff = 0, maxLen = 0;
+        for (int i = 0; i < a1.length; i++) {
+            diff += a1[i] - a2[i];
+            if (mp.containsKey(diff)) maxLen = Math.max(maxLen, i - mp.get(diff));
+            else mp.put(diff, i);
         }
-        return res;
+        return maxLen;
     }
 }
