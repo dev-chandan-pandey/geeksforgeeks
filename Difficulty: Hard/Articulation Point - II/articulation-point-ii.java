c466cdeda08c86959e173f1dad131493c44f7da4
dev-chandan-pandey <147697821+dev-chandan-pandey@users.noreply.github.com>
2026-03-28 21:42:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Articulation Point - II/articulation-point-ii.java
+++ Difficulty: Hard/Articulation Point - II/articulation-point-ii.java
@@ -1,94 +1,64 @@
-//{ Driver Code Starts
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        while (tc-- > 0) {
-            int V = sc.nextInt();
-            int E = sc.nextInt();
-            int[][] edges = new int[E][2];
-            for (int i = 0; i < E; i++) {
-                edges[i][0] = sc.nextInt();
-                edges[i][1] = sc.nextInt();
-            }
-
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.articulationPoints(V, edges);
-            Collections.sort(ans);
-            for (int val : ans) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-
-
 class Solution {
-    static ArrayList<Integer> articulationPoints(int V, int[][] edges) {
+    private int timer = 1;
+
+    public ArrayList<Integer> articulationPoints(int V, int[][] edges) {
+        // 1. Build Adjacency List
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < V; i++) {
-            adj.add(new ArrayList<>());
-        }
+        for (int i = 0; i < V; i++) adj.add(new ArrayList<>());
         for (int[] edge : edges) {
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
 
-        ArrayList<Integer> articulationPoints = new ArrayList<>();
-        int[] disc = new int[V];
-        int[] low = new int[V];
-        int time = 0;
-        boolean[] visited = new boolean[V];
-        boolean[] isAP = new boolean[V];
-
+        // 2. Initialize tracking arrays
+        int[] tin = new int[V];    // Time of insertion
+        int[] low = new int[V];    // Lowest time reachable
+        int[] vis = new int[V];    // Visited status
+        boolean[] mark = new boolean[V]; // Mark identified APs
+        
+        // 3. Handle disconnected components
         for (int i = 0; i < V; i++) {
-            if (!visited[i]) {
-                dfs(i, -1, disc, low, visited, isAP, time, adj);
+            if (vis[i] == 0) {
+                dfs(i, -1, vis, tin, low, mark, adj);
             }
         }
 
+        // 4. Collect and sort results
+        ArrayList<Integer> ans = new ArrayList<>();
         for (int i = 0; i < V; i++) {
-            if (isAP[i]) {
-                articulationPoints.add(i);
-            }
+            if (mark[i]) ans.add(i);
         }
 
-        if (articulationPoints.isEmpty()) {
-            articulationPoints.add(-1);
-        }
-        Collections.sort(articulationPoints);
-        return articulationPoints;
+        if (ans.size() == 0) ans.add(-1);
+        return ans;
     }
 
-    private static void dfs(int u, int p, int[] disc, int[] low, boolean[] visited, boolean[] isAP, int time, ArrayList<ArrayList<Integer>> adj) {
-        visited[u] = true;
-        disc[u] = low[u] = ++time;
-        int children = 0;
+    private void dfs(int node, int parent, int[] vis, int[] tin, int[] low, 
+                     boolean[] mark, ArrayList<ArrayList<Integer>> adj) {
+        vis[node] = 1;
+        tin[node] = low[node] = timer++;
+        int child = 0;
 
-        for (int neighbor : adj.get(u)) {
-            if (neighbor == p) continue;
-
-            if (visited[neighbor]) {
-                low[u] = Math.min(low[u], disc[neighbor]);
-            } else {
-                dfs(neighbor, u, disc, low, visited, isAP, time, adj);
-                low[u] = Math.min(low[u], low[neighbor]);
-                if (low[neighbor] >= disc[u] && p != -1) {
-                    isAP[u] = true;
+        for (Integer it : adj.get(node)) {
+            if (it == parent) continue;
+            if (vis[it] == 0) {
+                dfs(it, node, vis, tin, low, mark, adj);
+                low[node] = Math.min(low[node], low[it]);
+                // Check AP condition
+                if (low[it] >= tin[node] && parent != -1) {
+                    mark[node] = true;
                 }
-                children++;
+                child++;
+            } else {
+                // Back-edge: Update low value with ancestor's discovery time
+                low[node] = Math.min(low[node], tin[it]);
             }
         }
-
-        if (p == -1 && children > 1) {
-            isAP[u] = true;
+        // Root case
+        if (child > 1 && parent == -1) {
+            mark[node] = true;
         }
     }
 }
